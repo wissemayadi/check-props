ee9dc01eda42e424348d48540c3d1389a0c7b843
wissem <46422516+wissemayadi@users.noreply.github.com>
2022-02-23 14:02:31 +0100

    Update Cards.js

--- src/profile/Cards.js
+++ src/profile/Cards.js
@@ -1,21 +1,5 @@
 import React from 'react';
-// import {Card,Button} from 'react-bootstrap';
-// import { Alert } from 'reactstrap';
-// const Cards = () => {
-//     return (
-//       <Card style={{ width: "18rem", margin: "40px 0 40px 0" }}>
-//         <Card.Img variant="top" />
-//         <Card.Body>
-//           <Card.Title></Card.Title>
-//           <Card.Title></Card.Title>
-//           <Card.Text></Card.Text>
-//           <Button variant="primary" >
-//             Say Hi
-//           </Button>
-//         </Card.Body>
-//       </Card>
-//     );
-//   };
+
 const Cards = ({Firstname}) => {
   const handleClick = (x) => {
   
